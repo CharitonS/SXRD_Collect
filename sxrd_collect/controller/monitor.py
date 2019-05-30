931298d1a3646a792f0117b1da143c2f5ccf7b1d
Eran Greenberg <erangre@gmail.com>
2019-05-30 15:54:07 -0500

    Added the new file naming options t oBMD branch

---
+++ sxrd_collect/controller/monitor.py
@@ -0,0 +1,10 @@
+epics_config = {
+    'detector_position_x': '13IDD:m8',
+    'detector_position_z': '13IDD:m84',
+    'detector_control': '13MARCCD2:cam1',
+    'detector_file': '13MARCCD2:TIFF1',
+    'sample_position_x': '13IDD:m81',
+    'sample_position_y': '13IDD:m83',
+    'sample_position_z': '13IDD:m82',
+    'sample_position_omega': '13IDD:m96',
+}
